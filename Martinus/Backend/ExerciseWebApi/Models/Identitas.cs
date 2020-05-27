76024f7a181bd7bafb69d5e0b8c85f76c211d360
Martinus123S <martinussitinjak225@gmail.com>
2020-05-28 02:56:34 +0700

    Membuat folder Martinus, dan memasukkan Backend dan frontend yang akan di develop

---
+++ Martinus/Backend/ExerciseWebApi/Models/Identitas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseWebApi.Models
+{
+    public class Identitas
+    {
+        public long Id { get; set; }
+        public string nama { get; set; }
+        public string tempattinggal { get; set; }
+        public string nomor { get; set; }
+        public string pekerjaan { get; set; }
+    }
+}
